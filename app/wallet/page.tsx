969ef1b9a7224bd12d2909825726061557d721ef
anilsworld2000 <anilsworld2000@gmail.com>
2025-11-17 16:20:35 +0530

    fixed counter bug of max value is not setting on tab

---
+++ app/wallet/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useSelectedDashboard } from "../hooks/SelectedDashboardContext"
+
+export default function Wallet() {
+    const { selectDashboard } = useSelectedDashboard();
+    selectDashboard('Wallet');
+    return (
+        <div>Wallet</div>
+    )
+}
